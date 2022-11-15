b7dea6b177f1e4e49ef59517b9d77f588ade13f8
MMulti <86416862+MMulti@users.noreply.github.com>
2022-11-15 17:39:29 -0300

    Add files via upload

--- asset.js
+++ asset.js
@@ -1,43 +1,42 @@
 
-    let lugar ;
-    let fecha_hora ;
-    let cantidad ;
-    const reservas = [];  
-    let Line; 
-  
+let Line; 
+var reservas = [];
+let contador = 0; 
 
-do{
+function add() {
 
-    IngresoDatos();
-    
-    var next = confirm("¿Desea realizar otra reserva?");
-    
-}while( next == true )
+localStorage.setItem("Cantidad Maxima", JSON.stringify(2));
+IngresoDatos();
+//PrintData();
 
-PrintData();
+}
 
-function IngresoDatos(){
+function IngresoDatos() {
 
-     lugar = prompt("Ingrese el establecimiento:");
-     fecha_hora = prompt("Ingrese el dia y horario:");
-     cantidad = prompt("Ingrese la cantidad de personas");
+    contador = contador + 1 
+    let max = localStorage.getItem("Cantidad Maxima");
 
-     Line = lugar + " " + fecha_hora  + " " + cantidad ;
-
+if (contador <= max) {
+    let lugar = document.getElementById("Nombre");
+    let fecha_hora = document.getElementById("Horario");
+    let cantidad = document.getElementById("Personas");
+    Line = lugar.value + " el día " + fecha_hora.value  + " para " + cantidad.value + " personas ";
     reservas.push(Line)
-
     alert("Reserva realizada");
     
+}else 
+
+alert("Máximo Numero de reservas alcanzado");
 
 }
 
-function PrintData(){
+function PrintData() {
    
-
-   for(let index = 0 ; index < reservas.length ; index++)
-    {
-        document.write("<br/> " + reservas[index]);
-    }
-    
+    var reservas2 = [];
+    var reserva; 
+    reservas.forEach((reserva)=>{reservas2.push("<tr><td>"+reserva+"</td>")});
+    var output = document.querySelector("#output tbody");
+    output.innerHTML = reservas2;  
 
 }
+
